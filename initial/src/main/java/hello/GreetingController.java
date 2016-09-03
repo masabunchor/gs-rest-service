85c9fb4932523413ecfae1aa5c5923e1a8bb3f2e
masabun <masabunbunbun@gmail.com>
2016-09-03 18:02:10 +0900

    name=masabunの場合の判定を追加

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -10,11 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+    private static final String template2 = "Hi, %s!";
+
     private final AtomicLong counter = new AtomicLong();
+
+    private final AtomicLong counter2 = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+        if(name.equals("masabun")){
+        	return new Greeting(counter2.incrementAndGet(),
+                    String.format(template2, name));
+        } else {
+        	return new Greeting(counter.incrementAndGet(),
+        			String.format(template, name));
+        }
     }
 }
